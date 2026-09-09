a8fac8d216013870fbdf4675d6d24cb6794bf6fa
Joaquim Jaime <jqmjaime782@gmail.com>
2026-09-09 15:15:43 +0100

    Substituir logotipo Payload pelo logotipo da ASSEF no painel admin

---
+++ cms/src/components/Logo.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export function Logo() {
+  return (
+    <img
+      src="/logo-assef.png"
+      alt="ASSEF"
+      style={{ height: '42px', width: 'auto' }}
+    />
+  )
+}
+
+export function Icon() {
+  return (
+    <img
+      src="/logo-assef.png"
+      alt="ASSEF"
+      style={{ height: '24px', width: 'auto' }}
+    />
+  )
+}
